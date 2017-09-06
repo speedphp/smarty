ace8dd2bad60fb1ee1acc3ef293dae7d4b94f0d6
jake <jake@speedphp.com>
2017-09-06 15:28:41 +0800

    完成初步匹配和生成

--- smarty.js
+++ smarty.js
@@ -1,31 +1,37 @@
+const config = require("./lib/config")
+function Smarty(_config) {
+    var Smarty = new Object();
+    Smarty.config = config.defaults;
+    if (_config !== undefined) Smarty.config = config.merge(Smarty.config, _config);
 
-'use strict';
-var Smarty = function (_config) {
-    this._config = require("./lib/config")(_config);
+    Smarty.compile = function (html) {
+        return require("./lib/compile")(html, require("./lib/syntax"))
+    }
+
+    Smarty.readFile = function (filePath) {
+        return require("./lib/readFile")(filePath, this.config);
+    }
+
+    Smarty.render = function (file, options, callback) {
+        var filePath = Smarty.config.template_dir + file;
+        var html = Smarty.readFile(filePath);
+        var runable = Smarty.compile(html).join("");
+        var $SMARTY_STR = "";
+        eval(runable);
+        callback(null, $SMARTY_STR)
+    }
+
+
+    /**
+     * for expressjs
+     */
+    Smarty.express = function (filePath, options, callback) {
+        var contents = this.readFile(filePath);
+        var complied = this.compile(contents);
+        var render = this.render(complied, options)
+        callback(null, callback);
+    }
+
+    return Smarty;
 }
-Smarty.prototype.render = function () {
-
-}
-
-Smarty.prototype.compile = function () {
-
-}
-
-Smarty.prototype.readFile = function (filePath) {
-    return require("./lib/readFile")(filePath, this._config);
-}
-
-
-
-/**
- * for expressjs
- */
-Smarty.prototype.express = function (filePath, options, callback) {
-    var contents = this.readFile(filePath);
-    var complied = this.compile(contents);
-    var render = this.render(complied, options)
-    callback(null, callback);
-}
-
-
-module.exports = new Smarty()
+exports = module.exports = Smarty
--- test.js
+++ test.js
@@ -3,7 +3,10 @@ var app = function () {
 }
 
 app.prototype.render = function render(name, options, callback) {
-    var engine = require("./smarty.js");
+    var engine = new require("./smarty.js")({
+        "caching" : "helo"
+    }).render;
+
     engine("test.html", {
         "title" : "标题",
         "message" : "内容"
@@ -15,4 +18,5 @@ var toto = new app()
 toto.render("", "", function (err, rendered) {
     console.log(rendered)
 })
+
 
